20dd3a6d84ae49f7f3c02ce854762f063539eaf7
Vincent Uhlmann <contact@vuhlmann.com>
2021-12-17 19:39:26 +0100

    Added powershell scripts

---
+++ MoveDuplicates.ps1
@@ -0,0 +1,37 @@
+﻿[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true)][AllowEmptyString()][String] $sourceDir,
+  [Parameter(Mandatory=$true)][AllowEmptyString()][String] $targetDir
+)
+
+If ([string]::IsNullOrEmpty($sourceDir) -or !(Test-Path $sourceDir) -or [string]::IsNullOrEmpty($targetDir) -or!(Test-Path $targetDir)){
+    Write-Warning "Invalid source or target directory"
+    Exit
+}
+
+Write-Output "Scanning folders..."
+
+$dirs = Get-ChildItem $sourceDir -Directory
+
+Write-Output "Moving files..."
+
+foreach($dir in $dirs) {
+    $duplicates = Get-ChildItem $dir.FullName -File -ErrorAction SilentlyContinue | Get-FileHash | Group-Object -Property Hash | Where-Object Count -GT 1
+
+    foreach($duplicate in $duplicates)
+    {
+        foreach($duplicatePath in ($duplicate.Group.Path | Select-Object -Skip 1))
+        {
+            $targetFolder = Join-Path $targetDir $dir.Name
+                
+            if(!(Test-Path -Path $targetFolder ))
+            {
+                new-item -ItemType Directory -Path $targetFolder | Out-Null
+            } 
+
+            Move-Item -Path $duplicatePath -Destination $targetFolder
+        }
+    }
+
+    Write-Output "Progress: $($dirs.IndexOf($dir) + 1)/$($dirs.Count)"
+}
---
+++ SortFilesByExtension.ps1
@@ -0,0 +1,48 @@
+﻿[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true)][AllowEmptyString()][String] $sourceDir,
+  [Parameter(Mandatory=$true)][AllowEmptyString()][String] $targetDir
+)
+
+If ([string]::IsNullOrEmpty($sourceDir) -or !(Test-Path $sourceDir) -or [string]::IsNullOrEmpty($targetDir) -or!(Test-Path $targetDir)){
+    Write-Warning "Invalid source or target directory"
+    Exit
+}
+
+Write-Output "Scanning folders..."
+
+$dirs = Get-ChildItem $sourceDir -Directory
+
+Write-Output "Moving files..."
+
+foreach($dir in $dirs) {
+    $files = Get-ChildItem $dir.FullName -File
+
+    foreach($file in $files) {
+        $newFolderPath = Join-Path -Path $targetDir -ChildPath $file.Extension
+        $newFilePath = Join-Path -Path $newFolderPath -ChildPath "$($file.BaseName)$($file.Extension)"
+
+        if(!(Test-Path -Path $newFolderPath ))
+        {
+            new-item -ItemType Directory -Path $newFolderPath | Out-Null
+        }
+
+        $trigger = $false
+        $counter = 1
+
+        Do {
+            If (!(Test-Path -Path $newFilePath)) {
+                $trigger = $true
+            } Else {
+                $newFilePath = Join-Path -Path $newFolderPath -ChildPath "$($file.BaseName + $counter)$($file.Extension)"
+                $counter++
+            }
+
+        } Until ($trigger)
+      
+
+        Copy-Item -Path $file.FullName -Destination $newFilePath
+    }
+
+    Write-Output "Progress: $($dirs.IndexOf($dir) + 1)/$($dirs.Count)"
+}
